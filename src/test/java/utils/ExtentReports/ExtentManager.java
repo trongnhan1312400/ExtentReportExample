4771b2fa1e57b37f66f0ba0d796a6b51f182b322
nhan.trong.nguyen <nhan.trong.nguyen@logigear.com>
2018-03-20 17:02:29 +0700

    Initial Commit
    Signed-off-by: nhan.trong.nguyen <nhan.trong.nguyen@logigear.com>

--- src/test/java/utils/ExtentReports/ExtentManager.java
+++ src/test/java/utils/ExtentReports/ExtentManager.java
@@ -1,5 +1,7 @@
 package utils.ExtentReports;
 
+import java.io.File;
+
 import com.relevantcodes.extentreports.ExtentReports;
 
 //OB: ExtentReports extent instance created here. That instance can be reachable by getReporter() method.
@@ -8,11 +10,12 @@ public class ExtentManager {
 
     private static ExtentReports extent;
 
-    public synchronized static ExtentReports getReporter(){
+	public synchronized static ExtentReports getReporter(){
         if(extent == null){
             //Set HTML reporting file location
             String workingDir = System.getProperty("user.dir");
-            extent = new ExtentReports(workingDir+"\\ExtentReports\\ExtentReportResults.html", true);
+            extent = new ExtentReports(workingDir+"\\ExtentReports\\ExtentReportResults.html", false);
+            extent.loadConfig(new File("config.xml"));
         }
         return extent;
     }
